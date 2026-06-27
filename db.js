3fbe3bd6445238727b9dfbc2751a233a47039617
bazben <bazben498@gmail.com>
2026-06-27 09:16:31 +0000

    Update db.js

--- db.js
+++ db.js
@@ -2,10 +2,12 @@ const mysql = require('mysql2');
 
 //connexion à mysql de XAMPP
 const db = mysql.createConnection({
-   host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'exams'
+   host: process.env.DB_HOST || 'localhost',
+   port: process.env.DB_PORT || 3000,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'exams' || 'railway',
+   ssl: process.env.DB_HOST ? {rejectUnauthorized: false } : false
 });
 
 //Test de connexion
