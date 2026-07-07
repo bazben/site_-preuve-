d6c840d2791d2117572bbe5a71545139a6ba3e09
bazben <bazben498@gmail.com>
2026-07-07 18:31:34 +0000

    Update BAC1.js

--- public/BAC1.js
+++ public/BAC1.js
@@ -1,3 +1,8 @@
+document.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location = "index.html";
+});
+
 const form = document.getElementById('search');
 const div = document.getElementById('resultats');
 
