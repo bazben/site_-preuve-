60e92e93a602c35d749cebd393e123834b01dfa6
bazben <bazben498@gmail.com>
2026-07-22 18:11:56 +0000

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,3 +4,6 @@ self.options = {
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => self.clients.claim());
