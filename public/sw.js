b4d0780cd8bbb1e2790f336885073a5787c91cf9
bazben <bazben498@gmail.com>
2026-07-24 20:59:20 +0000

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -5,5 +5,35 @@ self.options = {
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw');
 
-self.addEventListener('install', e => self.skipWaiting());
-self.addEventListener('activate', e => self.clients.claim());
+
+// Cashe
+const CACH_NAME = 'bazben-cashe-v1';
+const urls = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+// Installation
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACH_NAME)
+        .then(cache => cache.addAll(urls))
+    );
+    self.skipWaiting();
+});
+
+// Activation
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
+});
+
+// Activation
+self.addEventListener('fetch', event => {
+   event.respondWith(
+   caches.match(event.request)
+       .then(response => response || fetch(event.request))
+   ); 
+});
