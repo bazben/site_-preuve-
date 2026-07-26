7819d2bf94549bcd0ee3c40d2d5335295a5baf28
bazben <bazben498@gmail.com>
2026-07-26 08:25:28 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const db = require('./db');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
