16e455c6edafb21008fb3f00c04d0edb6c46790b
bazben <bazben498@gmail.com>
2026-07-26 14:58:19 +0000

    Add files via upload

---
+++ public/connexion.js
@@ -0,0 +1,81 @@
+const form = document.getElementById('form');
+const message = document.getElementById('message');
+const exam = document.getElementById('examen');
+const ccn = document.getElementById('ccn');
+
+examen.addEventListener('change', () => {
+   if (exam.value !== 'BEPC') {
+       ccn.style.display = 'grid'; 
+   } else {
+       ccn.style.display = 'none';
+   }
+});
+
+form.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const nom =  document.getElementById('nom');
+    const prenom = document.getElementById('prenom');
+    const email = document.getElementById('email');
+    const password = document.getElementById('password');
+    
+    [nom, prenom, password, email].forEach(input => {
+       input.addEventListener('input', () => {
+          input.style.border = '1px solid #ccc';
+           input.style.backgroundColor = 'white';
+       });
+    });
+    
+   [nom, prenom, password, email].forEach(tt => {
+       if (!tt.value.trim()) {
+           tt.style.border = 'double red';
+       }
+       
+   });
+    if (!nom.value.trim() || !prenom.value.trim() || !email.value.trim() || !password.value.trim()) {
+        message.style.color = 'red';
+        message.innerText = 'remplissez le champ vide';
+        message.style.backgroundColor = 'black';
+        return;
+    }
+    
+   const data = {
+       nom: nom.value.trim(),
+       prenom: prenom.value.trim(),
+       email: email.value.trim(),
+       password: password.value.trim(),
+       examen: exam.value.trim(),
+       serie: document.getElementById('serie').value.trim()
+   };
+    message.innerText = "Loading...";
+    message.style.color = 'black';
+    
+    try {
+        const res = await fetch('https://bazben-site-preuve.onrender.com/register', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json'  
+            },
+            body: JSON.stringify(data)
+        });
+        const results = await res.json();
+        
+        if(res.ok) {
+            message.style.color = 'green';
+            message.innerText = 'connexion réussie! Token: ' + results.token.substring(0,20) + '...';
+        }else {
+            message.style.color = 'red';
+            message.innerText = 'Erreur: ' + (results.message || results.error || 'Une erreur est survenue');
+        }
+    }catch(err) {
+        message.style.color = 'red';
+        message.innerText = 'Erreur de connexion';
+        console.log(err);
+    }
+});
+
+form.addEventListener('keydown', e => {
+   if (e.key === 'enter') {
+       e.preventDefault();
+       form.requestSubmit();
+   } 
+});
