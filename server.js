1fc20ab0c7a8b3f4214cdd6a137a787870955785
bazben <bazben498@gmail.com>
2026-07-26 08:28:51 +0000

    Update bcrypt to bcryptjs in server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cors = require('cors');
 const db = require('./db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const app = express();
