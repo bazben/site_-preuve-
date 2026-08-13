11d7330e31ab8a10c147cb28946b471c6a60cd12
bazben <bazben498@gmail.com>
2026-08-13 21:15:20 +0000

    Update BAC1.js

--- public/BAC1.js
+++ public/BAC1.js
@@ -34,7 +34,7 @@ form.addEventListener('submit', async (e) => {
         <h3>${epreuve.matiere}</h3><br>
          <iframe src="https://docs.google.com/gview?url=${encodeURIComponent(epreuve.fichier_url)}&embedded=true" width="100%" height="200%"></iframe>
          <button onclick="download('${epreuve.fichier_url}', '${epreuve.matiere}')">Télécharger</button>
-        <script>
+        <div><script>
   atOptions = {
     'key' : 'a20c788f60a9012f0728d80f344c173a',
     'format' : 'iframe',
@@ -43,7 +43,7 @@ form.addEventListener('submit', async (e) => {
     'params' : {}
   };
 </script>
-<script src="https://www.highperformanceformat.com/a20c788f60a9012f0728d80f344c173a/invoke.js"></script>
+<script src="https://www.highperformanceformat.com/a20c788f60a9012f0728d80f344c173a/invoke.js"></script></div>
 
         `;
             });
