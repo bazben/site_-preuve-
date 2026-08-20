0700b1011fdabf8b833bc778b1048169504dc64f
bazben <bazben498@gmail.com>
2026-08-20 15:21:24 +0000

    Implement news fetching and update click events
    Added news fetching functionality and updated event listeners.

--- public/index.js
+++ public/index.js
@@ -13,3 +13,32 @@ bepc.addEventListener('click', (e) => {
    e.preventDefault();
     window.location = "BEPC.html";
 });
+
+const g_div = document.getElementById('news');
+async function news() {
+   try {
+      const res = await fetch('https://bazben-site-preuve.onrender.com/news');
+   const news = await res.json();
+   news.data.forEach(d => {
+      const div = document.createElement('div');
+      div.className = 'news';
+      const a = document.createElement('a');
+      a.className = 'aa';
+   a.href = d.link;
+      const img = document.createElement('img');
+      img.src = d.img;
+      img.className = 'mag';
+      a.appendChild(img);
+      const h2 = document.createElement('h2');
+      h2.textContent = d.title;
+      a.appendChild(h2);
+      div.appendChild(a);
+      g_div.appendChild(div);
+      });
+   }catch(err) {
+      console.log("voici:", err);
+   }
+   
+}
+
+news();
