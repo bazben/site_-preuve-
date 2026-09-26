8c29fc7333ccd335b4de0d63fd7936645d43a2f3
bazben <bazben498@gmail.com>
2026-09-26 13:55:08 +0000

    Update server.js

--- server.js
+++ server.js
@@ -235,7 +235,7 @@ async function scraper2() {
 app.get('/news', async (req, res) => {
     const news1 = await scraper1();
     const news2 = await scraper2();
-     const news = JSON.parse(news1) + JSON.parse(news2);
+     const { news } = { JSON.parse(news1), JSON.parse(news2) }
     res.json({
        total: news2.length + news1.length,
         data: news 
