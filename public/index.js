ad1facc8cb4f290276a61acea22adc5d8b9472f3
bazben <bazben498@gmail.com>
2026-09-26 09:56:45 +0000

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,19 +1,3 @@
-const bac1 = document.getElementById("bac1");
-bac1.addEventListener('click', (e) => {
-   e.preventDefault();
-    window.location = "BAC1.html";
-});
-const bac2 = document.getElementById("bac2");
-bac2.addEventListener('click', (e) => {
-   e.preventDefault();
-    window.location = "BAC2.html";
-});
-const bepc = document.getElementById("bepc");
-bepc.addEventListener('click', (e) => {
-   e.preventDefault();
-    window.location = "BEPC.html";
-});
-
 const g_div = document.getElementById('news');
 async function news() {
    try {
