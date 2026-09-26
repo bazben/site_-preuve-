5aebdc599bb1a8768e70c5cb2fb649365fa99ee2
bazben <bazben498@gmail.com>
2026-09-26 14:14:38 +0000

    Update news function to handle multiple news categories

--- public/index.js
+++ public/index.js
@@ -1,9 +1,26 @@
+
 const g_div = document.getElementById('news');
 async function news() {
    try {
       const res = await fetch('https://bazben-site-preuve.onrender.com/news');
    const news = await res.json();
-   news.data.forEach(d => {
+   news.data.news1.forEach(d => {
+      const div = document.createElement('div');
+      div.className = 'news';
+      const a = document.createElement('a');
+      a.className = 'aa';
+   a.href = d.link;
+      const img = document.createElement('img');
+      img.src = d.img;
+      img.className = 'mag';
+      a.appendChild(img);
+      const h2 = document.createElement('h2');
+      h2.textContent = d.title;
+      a.appendChild(h2);
+      div.appendChild(a);
+      g_div.appendChild(div);
+      });
+       news.data.news2.forEach(d => {
       const div = document.createElement('div');
       div.className = 'news';
       const a = document.createElement('a');
